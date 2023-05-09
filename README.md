12f6f6c8d4eb2b27d564cf0faeadf5ff1dc1eb6a
ywenhao <37856060+ywenhao@users.noreply.github.com>
2023-05-09 20:44:39 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hover-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ktu7je)
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <div class="box">
+    <div class="item">1</div>
+    <div class="item">2</div>
+    <div class="item">3</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.active {
+  background: red;
+}
+
+.default {
+  background: gray;
+}
+
+.box {
+  width: 200px;
+  display: flex;
+  gap: 10px;
+  .item {
+    flex: 1;
+    height: 50px;
+    cursor: pointer;
+    @extend .default;
+  }
+}
+
+// hover 部分
+.box {
+  .item:nth-child(2) {
+    @extend .active;
+  }
+  &:hover {
+    .item {
+      @extend .default;
+    }
+    .item:hover {
+      @extend .active;
+    }
+  }
+}
+</style>
